ef5a4e3e01ee000cd373dd3dc548ee1bf1bcf4b9
lordbabo <slaven55555@gmail.com>
2020-04-17 17:14:54 +0200

    Add project files.

---
+++ ConsoleApp2/Die.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ConsoleApp2
+{
+    class Die
+    {
+        private int numberOfSides;
+        private Random randomGenerator;
+        public Die(int numberOfSides)
+        {
+            this.numberOfSides = numberOfSides;
+            this.randomGenerator = new Random();
+        }
+        public Die(int numberOfSides, Random randomGenerator) //zadatak 2
+        {
+            this.numberOfSides = numberOfSides;
+            this.randomGenerator = randomGenerator;
+        }
+
+
+        public int Roll()
+        {
+            int rolledNumber = randomGenerator.Next(1, numberOfSides + 1);
+            return rolledNumber;
+        }
+
+    }
+}
---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DiceRoller diceRoller = new DiceRoller();
+            Random randomGenerator = new Random();
+            int i;
+            int numOfDice = 20;
+            for(i=0;i<numOfDice; i++)
+            {
+                diceRoller.InsertDie(new Die(6)); //zadatak 1
+                diceRoller.InsertDie(new Die(6, randomGenerator)); //zadatak 2
+
+            }
+            IList<int> resultsOfRolling = diceRoller.GetRollingResults();
+            diceRoller.RollAllDice();
+            foreach (int result in resultsOfRolling)
+            {
+                Console.WriteLine(result);
+            }
+        }
+    }
+}
